1e48e21e006119f771bb691917fc5652bc0d2f77
alxcode <ing.gramos@outlook.com>
2017-11-30 22:24:51 -0600

    files add

---
+++ DemoWebAppPy/enviroment/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Repos\django\DemoPy_Continous_Integration\DemoWebAppPy\enviroment\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
